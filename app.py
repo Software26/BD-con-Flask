f9867a862f1a41515b987ec038bd939800e32797
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-04-13 18:14:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+#--- Aqui tenemos la ruta de donde esta el archivo el print es parque lo veamos por cosola
+directorio = os.path.abspath(os.path.dirname(__file__))
+print(directorio)
+#----------------------------------------
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directorio,'datos.sqlite')
+app.config['SQLALCHEMY_tract'] 
