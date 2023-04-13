00d26e60a9aa9a777dc60ad34f821642c6429d78
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-04-13 18:46:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(directorio,'
 app.config['SQLALCHEMY_TRACK_MOODIFICACIONS'] = False
 
 basededatos = SQLAlchemy(app)
+
+#creaccion del modelo o base de datos
+
+class Persona(basededatos.Model):
+    __tablename__='Personas'
+    
+    id = basededatos.Column(basededatos.Integer,primary_key = True)
+    nombre = basededatos.Column(basededatos.Texto)
+    edad = basededatos.Column(basededatos.Integer)
+    
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+       
+    def __repr__(self):
+        texto = "Persona : nombre ={} y edad={}".format()
+        return super().__repr__() 
+        
+        super().__init__()
