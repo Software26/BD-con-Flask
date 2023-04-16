ff4da81260f77b2a0aba075eb0b9bc937a5ffb59
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-04-16 12:40:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,13 +20,18 @@ class Persona(basededatos.Model):
     __tablename__='Personas'
     
     id = basededatos.Column(basededatos.Integer,primary_key = True)
-    nombre = basededatos.Column(basededatos.Texto)
+    nombre = basededatos.Column(basededatos.Text)
     edad = basededatos.Column(basededatos.Integer)
+    color = basededatos.Column(basededatos.Text)
     
-    def __init__(self,nombre,edad):
+    def __init__(self,nombre,edad,color):
         self.nombre = nombre
         self.edad = edad
        
     def __repr__(self):
-        texto = "Persona : nombre ={} y edad={}".format(self.nombre,self.edad)
+        texto = "Persona : nombre ={} y edad={}".format(self.nombre,self.edad,self.color)
         return texto
+    
+if __name__ == '__main__':
+    with app.app(debug=True):
+        app.run()
